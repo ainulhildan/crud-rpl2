26c3c19b329a0e878974c12e3245c6042519973c
ainulhildan <122774910+ainulhildan@users.noreply.github.com>
2023-10-11 15:10:51 +0700

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Contracts\View\view;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use App\Models\Post;
+use Illuminate\Support\Facades\Storage;
+
+class PostController extends Controller
+{
+    public function index()
+{
+    // get posts
+    $posts = post::latest()->paginate(5);
+
+    // render view with posts
+    return view('posts.index', compact('posts'));
+    //
+    }
+
+
+public function create()
+{
+    return view('posts.create');
+}
+
+/**
+ * store
+ * 
+ * @param Request $request
+ * @return void
+ */
+public function store(Request $request): RedirectResponse
+{
+    // validate form
+    $this -> validate($request, [
+        'image'  => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        'title'  => 'required|min:5',
+        'content' => 'required|min:10'
+    ]);
+    // upload image
+    $image = $request->file('image');
+    $image->storeAs('public/posts', $image->hashName());
+    // create post
+    post::create([
+        'image'      => $image->hashName(),
+        'title'      => $request->title,
+        'content'    => $request->content
+    ]);
+    return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Disimpan!']);
+}
+public function edit(Post $post): View
+{
+    return view('posts.edit', compact('post'));
+}
+
+
+public function update(Request $request, Post $post): RedirectResponse
+{
+    // validate form
+    $this -> validate($request, [
+        'image'  => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        'title'  => 'required|min:5',
+        'content' => 'required|min:10'
+    ]);
+    // check if image is uploaded
+    if ($request->hasFile('image')){
+        // upload new image
+        $image = $request->file('image');
+        $image->storeAs('public/posts/', $image->hashName());
+        // delete old image
+        Storage::delete('public/posts'.$post->$image);
+        // update post with new image
+        $post->update([
+            'image'   => $image->hashName(),
+            'title'   => $request->title,
+            'content' => $request->content
+        ]);
+    }else{
+
+        // update post without image
+        $post->update([
+            'title'  => $request->title,
+            'content'=> $request->content,
+        ]);
+    }
+    // reqirect to index
+    return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Diubah!']);
+}
+    public function destroy(Post $post): RedirectResponse
+    {
+        // delete image
+        Storage::delete('public/posts'.$post->image);
+        // delete post
+        $post->delete();
+        // redirect to index
+        return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Dihapus!']);
+    }
+
+    public function show(string $id)
+    {
+        // get post by ID
+        $post = Post::findOrFail($id);
+        // render view with post
+        return view('posts.show', compact('post'));
+    }
+}
